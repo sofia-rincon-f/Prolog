2aea089cb51ab73e971868673f7fbd64f7b8ec0c
Sofia <s.rincon4@utp.edu.co>
2026-03-03 20:42:24 -0500

    Add Fibonacci function in Prolog

---
+++ fibonacci.pl
@@ -0,0 +1,12 @@
+% Casos base
+fibonacci(0, 0).
+fibonacci(1, 1).
+
+% Caso recursivo
+fibonacci(N, F) :-
+    N > 1,
+    N1 is N - 1,
+    N2 is N - 2,
+    fibonacci(N1, F1),
+    fibonacci(N2, F2),
+    F is F1 + F2.
