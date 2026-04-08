c226aae8732ce96b737635ee1bd611db428b1c8b
Sofia <s.rincon4@utp.edu.co>
2026-04-07 21:41:19 -0500

    Refactor poligono and add poligono_cerrado predicates

--- Colas/clase1.pl
+++ Colas/clase1.pl
@@ -9,6 +9,10 @@ conectados(S1, S2):-
     S1=..[segmento,_,P],
     S2=..[segmento,P,_].
 
+	%conectados(segmento(punto(0,0),punto(0,1)),segmento(punto(0,1),punto(1,0))).
+	
+	%=.. unificación de estructuras en una lista
+
 conectados_v2(segmento(_,P),segmento(P,_)). 
 
 %paso base
@@ -20,4 +24,17 @@ poligono([S1, S2]):-
 poligono([S1][S2|Tail]) 
 	conectados(S1,S2),
 	append([S2], Tail, nombre), %append adiciona un elemento
-	poligono(nombre).
+	poligono(Nombre).
+	
+%consulta 
+
+
+poligono_cerrado([S1|Tail]):-
+	proper_length(Tail, Len),
+	len>1,
+	append([S1], Tall, Nombre),
+	poligono(Nombre, L),
+	conectados(L, S1). 
+	
+%consulta
+	
